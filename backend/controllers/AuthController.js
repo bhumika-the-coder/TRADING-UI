43db17b30d5b19e0f75b4d7d67a2185e74b06b2b
BHUMIKA <bhumikaprajapatiofficial@gmail.com>
2026-08-24 13:58:47 +0530

    fixed same site cookie problem

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -39,17 +39,18 @@ module.exports.Login = async (req, res, next) => {
       return res.json({ message: "Incorrect password or email" });
     }
     const token = createSecretToken(user._id);
+
     res.cookie("token", token, {
-      httpOnly: false,
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
     });
-    res
-      .status(201)
-      .json({
-        message: "User logged in successfully",
-        success: true,
-        token,
-        user,
-      });
+
+    res.status(201).json({
+      message: "User logged in successfully",
+      success: true,
+      user,
+    });
     next();
   } catch (error) {
     console.error(error);
