258f3aaba20a55617a52f1442228cb3dd87fa999
BHUMIKA <bhumikaprajapatiofficial@gmail.com>
2026-08-24 13:40:30 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -251,8 +251,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
+      "https://zerodha-ui-clone-home.vercel.app/",
+      "https://zerodha-ui-clone.vercel.app/",
     ],
     credentials: true,
   })
