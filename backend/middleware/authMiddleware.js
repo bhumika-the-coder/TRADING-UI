3c0399e5f04f226c43864cb7507c05def7677bd3
BHUMIKA <bhumikaprajapatiofficial@gmail.com>
2026-08-24 20:51:12 +0530

    changed cors PACKAGE

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-/* const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 
 const authMiddleware = (req, res, next) => {
@@ -23,34 +23,4 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = authMiddleware; */
-
-
-const authMiddleware = (req, res, next) => {
-  try {
-    console.log("COOKIES:", req.cookies);
-
-    const token = req.cookies.token;
-
-    if (!token) {
-      console.log("TOKEN NOT FOUND");
-      return res.status(401).json({
-        message: "Unauthorized",
-      });
-    }
-
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-
-    console.log("DECODED USER:", decoded);
-
-    req.user = decoded;
-
-    next();
-  } catch (err) {
-    console.log("JWT ERROR:", err);
-
-    return res.status(401).json({
-      message: "Invalid or expired token",
-    });
-  }
-};
+module.exports = authMiddleware;
